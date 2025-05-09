32d92a3abbe1abebb961d71ba2eb9ede7c2062a0
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-09 12:50:20 +0700

    Update init_db.py

--- app/init_db.py
+++ app/init_db.py
@@ -1,5 +1,5 @@
 # init_db.py
-from app.database import Base, engine
-from app.models import user, obat, stock, penyakit, pemakaian
+from database import Base, engine
+from models import user, obat, stock, penyakit, pemakaian
 
 Base.metadata.create_all(bind=engine)
