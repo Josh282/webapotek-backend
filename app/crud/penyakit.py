77430febcce90a7ebd7fc112e8d2682e541fad7e
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-10 09:14:43 +0700

    Update penyakit.py

--- app/crud/penyakit.py
+++ app/crud/penyakit.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from models.penyakit import Penyakit
+from app.models.penyakit import Penyakit
 
 def get_all_penyakit(db: Session):
     return db.query(Penyakit).all()
