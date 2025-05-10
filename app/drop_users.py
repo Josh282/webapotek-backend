1d34a335bb7c6cc1e39d440733dcb05187117dfd
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-10 09:19:04 +0700

    Update drop_users.py

--- app/drop_users.py
+++ app/drop_users.py
@@ -1,5 +1,5 @@
-from database import engine
-from models.user import User
+from app.database import engine
+from app.models.user import User
 
 print("⚠️  Dropping 'users' table...")
 User.__table__.drop(engine)
