b5c68896f36779915838a8d725438602cc2ec75f
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-11 00:42:07 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = "sqlite:///./app.db"
+DATABASE_URL = "sqlite:///app.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
