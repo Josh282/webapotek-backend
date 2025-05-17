c60e35db53464d6b6ce4c8fa4066482e39685aba
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-17 07:44:07 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,6 +29,7 @@ app.include_router(pemakaian.router, prefix="/pemakaian", tags=["Pemakaian"])
 app.include_router(forecast.router, prefix="/forecast", tags=["Forecast"])
 app.include_router(upload.router, prefix="/upload", tags=["Upload"])
 app.include_router(debug.router, prefix="/debug", tags=["Debug"])
+app.include_router(pemakaian.router, prefix="/manual", tags=["Input Manual"])
 
 
 
