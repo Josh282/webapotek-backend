e4ac1da55110e6816947f06e7383af6fc02b90d3
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-17 07:36:35 +0700

    Update pemakaian.py
    adjusting for pemakaian_raw models

--- app/crud/pemakaian.py
+++ app/crud/pemakaian.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models.pemakaian import Pemakaian
-from app.schemas.pemakaian import PemakaianIn
+from app.schemas.pemakaian import PemakaianIn, PemakaianRawCreate
+from app.models.pemakaian_raw import PemakaianRaw
 
 def tambah_pemakaian(db: Session, data: PemakaianIn):
     entry = Pemakaian(**data.dict())
@@ -8,6 +9,13 @@ def tambah_pemakaian(db: Session, data: PemakaianIn):
     db.commit()
     db.refresh(entry)
     return entry
+
+def create_raw(db: Session, data: PemakaianRawCreate):
+    record = PemakaianRaw(**data.dict())
+    db.add(record)
+    db.commit()
+    db.refresh(record)
+    return record
 
 def get_all_pemakaian(db: Session):
     return db.query(Pemakaian).all()
