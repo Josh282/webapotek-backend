1c0e1c81fea7b5a6193347b98ef0f169b5c9508c
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-19 06:02:53 +0700

    Update forecast.py

--- app/api/routes/forecast.py
+++ app/api/routes/forecast.py
@@ -22,3 +22,16 @@ def get_forecast(
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Forecast failed: {str(e)}")
 
+@router.get("/top15")
+def get_top15_forecast(
+    horizon: int = Query(1, ge=1, le=12),
+    db: Session = Depends(get_db),
+    credentials: HTTPAuthorizationCredentials = Security(security)
+):
+    verify_token(credentials.credentials)
+    try:
+        hasil = predict_from_pemakaian(db, horizon=horizon)
+        hasil_sorted = sorted(hasil, key=lambda x: x["jumlah"], reverse=True)[:15]
+        return {"forecast_top15": hasil_sorted}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Top15 Forecast Error: {str(e)}")
