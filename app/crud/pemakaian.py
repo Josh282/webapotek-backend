aab90c2c4b8f921a9757523ec0fa77ff5d5c2333
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-18 23:14:11 +0700

    Update pemakaian.py
    add funcion for aggregating raw data

--- app/crud/pemakaian.py
+++ app/crud/pemakaian.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import func
+from datetime import date
 from app.models.pemakaian import Pemakaian
 from app.schemas.pemakaian import PemakaianIn, PemakaianRawCreate
 from app.models.pemakaian_raw import PemakaianRaw
@@ -19,3 +21,28 @@ def create_raw(db: Session, data: PemakaianRawCreate):
 
 def get_all_pemakaian(db: Session):
     return db.query(Pemakaian).all()
+
+def aggregate_pemakaian_raw(db: Session, start_date: date, end_date: date):
+    data = (
+        db.query(
+            PemakaianRaw.nama_obat.label("nama_obat"),
+            func.strftime("%Y-%m", PemakaianRaw.tanggal).label("bulan"),
+            func.sum(PemakaianRaw.volume).label("jumlah")
+        )
+        .filter(PemakaianRaw.tanggal >= start_date, PemakaianRaw.tanggal <= end_date)
+        .group_by(PemakaianRaw.nama_obat, func.strftime("%Y-%m", PemakaianRaw.tanggal))
+        .all()
+    )
+
+    inserted = 0
+    for row in data:
+        new_record = Pemakaian(
+            namaobat=row.nama_obat,
+            bulan=row.bulan + "-01",
+            jumlah=row.jumlah
+        )
+        db.add(new_record)
+        inserted += 1
+
+    db.commit()
+    return inserted
