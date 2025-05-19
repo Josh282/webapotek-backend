d35afda5d2f2f98cd6bd4078424110a0aa5e2a7c
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-19 12:17:18 +0700

    Update upload.py
    Adjusting to the raw upload

--- app/api/routes/upload.py
+++ app/api/routes/upload.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
 from sqlalchemy.orm import Session
 import pandas as pd
 import io
-from app.models.pemakaian import Pemakaian
 from app.api.dependencies import get_db
+from app.models.pemakaian_raw import PemakaianRaw
 
 router = APIRouter()
 
@@ -13,7 +13,7 @@ async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db
         contents = await file.read()
         filename = file.filename.lower()
 
-        # Pilih loader berdasarkan ekstensi
+        # Baca file sebagai excel atau csv
         if filename.endswith(".xlsx"):
             df = pd.read_excel(io.BytesIO(contents))
         elif filename.endswith(".csv"):
@@ -22,21 +22,27 @@ async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db
         else:
             raise HTTPException(status_code=400, detail="File harus .csv atau .xlsx")
 
-        required_cols = {"namaobat", "total_volume", "bulan"}
+        # Rename kolom agar match dengan field database
+        df.rename(columns={
+            "tgl_resep": "tanggal",
+            "namaobat": "nama_obat",
+            "Penyakit Utama": "penyakit",
+            "pabrikan": "pabrik"
+        }, inplace=True)
+
+        # Validasi kolom wajib
+        required_cols = {"tanggal", "nama_obat", "penyakit", "merk", "jenis", "pabrik", "volume"}
         if not required_cols.issubset(set(df.columns)):
-            raise HTTPException(
-                status_code=400,
-                detail="File harus mengandung kolom: namaobat, total_volume, bulan"
-            )
+            raise HTTPException(status_code=400, detail=f"File harus mengandung kolom: {', '.join(required_cols)}")
 
+        # Pastikan format tanggal benar
+        df["tanggal"] = pd.to_datetime(df["tanggal"]).dt.date
+
+        # Simpan ke database
         for _, row in df.iterrows():
-            db.add(Pemakaian(
-                namaobat=row["namaobat"],
-                jumlah=int(row["total_volume"]),
-                bulan=pd.to_datetime(row["bulan"]).date()
-            ))
-
+            db.add(PemakaianRaw(**row.to_dict()))
         db.commit()
+
         return {"message": f"Upload sukses: {file.filename}", "rows": len(df)}
 
     except Exception as e:
