eaf10721fbd7e2adf4735198117ace6b495d79ef
Josh282 <33403181+Josh282@users.noreply.github.com>
2025-05-19 08:54:42 +0700

    Update pemakaian.py
    add top5 penyakit per month function

--- app/crud/pemakaian.py
+++ app/crud/pemakaian.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import func
+from sqlalchemy import extract, func
 from datetime import date
 from app.models.pemakaian import Pemakaian
 from app.schemas.pemakaian import PemakaianIn, PemakaianRawCreate
@@ -63,3 +63,27 @@ def get_top15_pemakaian_raw(db: Session):
     )
 
     return [{"obat": r.nama_obat, "jumlah": int(r.total_volume)} for r in result]
+
+def get_top5_penyakit_bulan_ini(db: Session):
+    from app.models.pemakaian_raw import PemakaianRaw
+
+    today = date.today()
+    bulan = today.month
+    tahun = today.year
+
+    result = (
+        db.query(
+            PemakaianRaw.penyakit,
+            func.sum(PemakaianRaw.volume).label("total")
+        )
+        .filter(
+            extract("month", PemakaianRaw.tanggal) == bulan,
+            extract("year", PemakaianRaw.tanggal) == tahun
+        )
+        .group_by(PemakaianRaw.penyakit)
+        .order_by(func.sum(PemakaianRaw.volume).desc())
+        .limit(5)
+        .all()
+    )
+
+    return [{"penyakit": r.penyakit, "jumlah": int(r.total)} for r in result]
